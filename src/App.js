1e028e09e781af2cfa16208be96bd21a92b05102
Zack Hayden <signbarn@gmail.com>
2020-05-18 03:39:50 -0400

    react-portfolio

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <Router>
         <TopNav />
-        <Route exact path="/" component={About} />
+        <Route exact path="/react-portfolio" component={About} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
